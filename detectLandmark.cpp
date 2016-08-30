0eba1ba28790e87089dce229e23e1ff77888ebfe
dyouakim <dina.youakim@gmail.com>
2016-08-30 13:23:59 +0200

    make landmarks detection more robust

--- detectLandmark.cpp
+++ detectLandmark.cpp
@@ -1,3 +1,14 @@
+
+
+
+/* The process to detect the landmarks needs to be more robust with varying light conditions;
+ * only then the idea to optimize the map when the loop closue is detected will work.
+ * Right now, the problem is with the light coming from the windows and the light reflected from the walls. 
+ * 
+ */
+
+
+
 #include "landmark_detection.h"
 
 
@@ -26,26 +37,69 @@ void landmarkDetection::imageReceivedCallback(const sensor_msgs::ImageConstPtr& 
 	Mat image = cv_ptr->image;
     timestamp = img->header.stamp;
 
+    // reject 350 pixels from below; 150 pixels from above
+    cv::Rect roi = cv::Rect(0, 150, image.cols, image.rows-350);
+    Mat resizedImage(image, roi);
+    imshow("resized image", resizedImage);
 
-    Mat imgH;
-    image.convertTo(imgH, -1, 0.5, 0);
 
-    //imshow("Contrast Reduced", imgH);
-    cv::GaussianBlur(imgH, imgH, Size(9,9), 0, 0);
+    Mat resizedImage_gray, threshold_output;
+    cvtColor(resizedImage, resizedImage_gray, CV_BGR2GRAY);
+    imshow("resizedImage_gray", resizedImage_gray);
+    
+    threshold(resizedImage_gray, threshold_output, 250, 400, THRESH_BINARY); 
+    imshow("threshold_output", threshold_output);                            
 
-    int erosion_size = 4;
-    Mat erosion_dst;
-    Mat erosion_element = getStructuringElement( MORPH_ELLIPSE,
-                                       Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                       Point( erosion_size, erosion_size ) );
-    /// Apply the erosion operation
-    erode( imgH, erosion_dst, erosion_element );
-    cv::GaussianBlur(erosion_dst, erosion_dst, Size(9,9), 0, 0);
+    int dilation_size = 5;
+    Mat dilation_dst;
+    Mat dilation_element = getStructuringElement( MORPH_RECT,
+                                       Size( 4*dilation_size + 1, 4*dilation_size+1 ),
+                                       Point( dilation_size, dilation_size ) );
+    /// Apply the dilation operation
+    dilate( threshold_output, dilation_dst, dilation_element );
+    cv::GaussianBlur(dilation_dst, dilation_dst, Size(9,9), 0, 0);
+    imshow( "Dilation Output", dilation_dst );
 
-    //imshow( "Erosion Output", erosion_dst );
+    /*vector<vector<Point> > contour;
+    std::vector<Vec4i> hierarch;
+    findContours(threshold_output, contour, hierarch, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    
+    //imshow("threshold_output", threshold_output);
 
-    Mat hsv;
-    cvtColor(erosion_dst,hsv,CV_BGR2HSV);
+    std::cout << "contour.size: " << contour.size() << std::endl;
+
+    vector<RotatedRect> minEllipse(contour.size());
+    vector<RotatedRect> minRect(contour.size());
+
+    for( int i = 0; i < contour.size(); i++ )
+    {
+        minRect[i] = minAreaRect(Mat(contour[i])); 
+        if(contour[i].size() > 5 )
+        { 
+            minEllipse[i] = fitEllipse(Mat(contour[i])); 
+        }
+    }
+
+    RNG rng(12345);
+    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    for( int i = 0; i< contour.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        // contour
+        drawContours( drawing, contour, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+        // ellipse
+        ellipse( drawing, minEllipse[i], color, 2, 8 );
+        // rotated rectangle
+        Point2f rect_points[4]; minRect[i].points( rect_points );
+        for( int j = 0; j < 4; j++ )
+            line( drawing, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
+    }
+
+    imshow( "drawing", drawing );
+    
+
+    /*Mat hsv;
+    //cvtColor(erosion_dst,hsv,CV_BGR2HSV);
 
     // Threshold the HSV image, keep only the red pixels
     Mat lower_red_hue_range;
@@ -67,7 +121,7 @@ void landmarkDetection::imageReceivedCallback(const sensor_msgs::ImageConstPtr& 
     /// Apply the dilation operation
     dilate( red_hue_image, dilation_dst, dilation_element );
     cv::GaussianBlur(dilation_dst, dilation_dst, Size(9,9), 0, 0);
-    imshow( "Dilation Output", dilation_dst );
+    //imshow( "Dilation Output", dilation_dst );
 
 
     cv::threshold(dilation_dst, dilation_dst, 200, 300, CV_THRESH_BINARY);
@@ -89,8 +143,10 @@ void landmarkDetection::imageReceivedCallback(const sensor_msgs::ImageConstPtr& 
 
     map.insert(std::pair<ros::Time,bool>(timestamp,landmarksDetected));
 
-    imshow( "Contours", dilation_dst );
-    imshow("Original Image", image);
+    //imshow( "Contours", dilation_dst );
+    //imshow("Image", image);*/
+
+
     cv::waitKey(3);
 
 }
